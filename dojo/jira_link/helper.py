338bdbf12f68b322d5d3cef06d71382d9d41eee5
Matt Schuman <matthew.schuman@glassdoor.com>
2023-04-24 07:37:27 -0500

    updated to not include duplicate tags

--- dojo/jira_link/helper.py
+++ dojo/jira_link/helper.py
@@ -534,7 +534,8 @@ def get_tags(obj):
         obj_tags = obj.tags.all()
         if obj_tags:
             for tag in obj_tags:
-                tags.append(str(tag.name))
+                if tag not in tags:
+                    tags.append(str(tag.name))
     return tags
 
 
